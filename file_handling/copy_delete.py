2ba786992e560468359a13d91136f9f3d26513cb
Azatbraza <132365947+Azatbraza@users.noreply.github.com>
2026-03-21 13:16:54 +0500

    Update copy_delete.py

---
+++ file_handling/copy_delete.py
@@ -0,0 +1,22 @@
+import shutil
+import os
+
+# Copy file
+shutil.copy("sample.txt", "sample_copy.txt")
+print("File copied.")
+
+# Create backup folder
+os.makedirs("backup", exist_ok=True)
+
+# Move file to backup
+shutil.copy("sample.txt", "backup/sample_backup.txt")
+print("Backup created.")
+
+# Delete file safely
+file_to_delete = "sample_copy.txt"
+
+if os.path.exists(file_to_delete):
+    os.remove(file_to_delete)
+    print("File deleted safely.")
+else:
+    print("File not found.")
