700f4733ae0572109478fcddd07eae81d5cbb040
Azatbraza <132365947+Azatbraza@users.noreply.github.com>
2026-03-21 13:15:22 +0500

    Update write_files.py

---
+++ file_handling/write_files.py
@@ -0,0 +1,5 @@
+with open("sample.txt", "w") as file:
+    file.write("Hello, this is sample data.\n")
+    file.write("This is the second line.\n")
+
+print("File created and written successfully.")
